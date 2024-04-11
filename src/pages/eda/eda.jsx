32160c872ea66e59d29202a1a922d24c6b02bdfd
webdeveloper510 <developerweb6@gmail.com>
2024-04-11 12:20:40 +0530

    app changes are done

--- src/pages/eda/eda.jsx
+++ src/pages/eda/eda.jsx
@@ -209,11 +209,11 @@ const Eda = () => {
                   {values.type == 'player' && <>
                     <div className="flex items-center justify-center pt-5 pb-5">
                       <div className="relative group w-full" ref={dropdownRef}>
-                        <button type="button" onClick={toggleDropdown} className=" py-3 player-list buttonPlayer  text-center focus:outline-none appearance-none inline-flex justify-center w-full px-4  text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm  ">
+                        <button type="button" onClick={toggleDropdown} className=" py-3 player-list buttonPlayer  text-center focus:outline-none appearance-none inline-flex justify-center w-full px-4  text-sm font-medium text-gray-700 border border-gray-300 rounded-md shadow-sm  ">
                           {values.playerName === "" ? 'player name' : <span className="mr-2">{playerType === "pitcher" ? pitcher.player_name[parseInt(values.playerName)] : batter.player_name[parseInt(values.playerName)]}</span>}
 
                         </button>
-                        {values.playerType != "" && <div className={`absolute right-0 mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 p-1 space-y-1 ${isOpen ? '' : 'hidden'} max-h-60  w-full`}>
+                        {values.playerType != "" && <div className={`absolute right-0 mt-2 rounded-md shadow-lg  ring-1 ring-black ring-opacity-5 p-1 space-y-1 ${isOpen ? '' : 'hidden'} max-h-60  w-full`}>
                           <input onChange={handleInputChange} className="block w-full px-4 py-2 text-gray-800 border rounded-md border-gray-300 focus:outline-none" type="text"
 
                             placeholder="Search Player Name" autoComplete="off" />
